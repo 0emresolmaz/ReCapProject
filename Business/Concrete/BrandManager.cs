8153635d9b55ced3cccd1117af472049b247b53d
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-02-25 22:24:11 +0300

    Aspect Validation

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -17,22 +19,13 @@ namespace Business.Concrete
         {
             _brandDal = brandDal;
         }
+
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
-            if (brand.BrandName.Length >= 2)
-            {
-                _brandDal.Add(brand);
-                Console.WriteLine(brand.BrandName + " " + Messages.BrandAdded);
-                return new SuccessResult(Messages.BrandAdded);
-
-                //Console.WriteLine("Araba modeli: {0} başarıyla eklendi\n", brand.BrandName);
-            }
-            else
-            {
-                Console.WriteLine(brand.BrandName + " " + Messages.BrandNameValid);
-                return new ErrorResult(Messages.BrandNameValid);
-                //Console.WriteLine("Araba modeli: {0} en az 2 karakter olmalıdır.\n",brand.BrandName);
-            }
+            _brandDal.Add(brand);
+            Console.WriteLine(brand.BrandName + " " + Messages.BrandAdded);
+            return new SuccessResult(Messages.BrandAdded);
         }
 
         public IDataResult<List<Brand>> GetAll()
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult();
+        }
+
+        public IDataResult<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IDataResult<List<User>> IUserService.GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll());
+        }
+    }
+}
